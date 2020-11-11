845554972a68095e1ed3135727d132f5c7b277e6
Lydia Rodrigues <11950402+MaichiLydia@users.noreply.github.com>
2020-11-11 01:42:58 -0300

    7o app - Chat

---
+++ lib/text_composer.dart
@@ -0,0 +1,64 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class TextComposer extends StatefulWidget {
+
+  TextComposer(this.sendMessage);
+  final Function({String text, File imgFile}) sendMessage;
+
+  @override
+  _TextComposerState createState() => _TextComposerState();
+}
+
+class _TextComposerState extends State<TextComposer> {
+  final TextEditingController _textController = TextEditingController();
+  bool _isComposing = false;
+
+  void _reset () {
+    _textController.clear();
+    setState(() {
+      _isComposing = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8),
+      child: Row(
+        children: <Widget>[
+          IconButton(
+              icon: Icon(Icons.photo_camera),
+              onPressed: () async {
+                final File imgFile =  await ImagePicker.pickImage(source: ImageSource.camera);
+                if (imgFile == null) return;
+                widget.sendMessage(imgFile: imgFile);
+              }),
+          Expanded(
+              child: TextField(
+                controller: _textController,
+                decoration:
+                InputDecoration.collapsed(hintText: "Enviar uma mensagem"),
+                onChanged: (text) {
+                  setState(() {
+                    _isComposing = text.isNotEmpty;
+                  });
+                },
+                onSubmitted: (text) {
+                  widget.sendMessage(text: text);
+                  _reset();
+                },
+              )),
+          IconButton(
+              icon: Icon(Icons.send),
+              onPressed: _isComposing ? () {
+                widget.sendMessage(text: _textController.text);
+                _reset();
+              } : null)
+        ],
+      ),
+    );
+  }
+}
